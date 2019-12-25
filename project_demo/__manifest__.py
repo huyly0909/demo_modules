dac948c615987eef58c2f484b78a170cabfed4b2
Hitman <hitman@Supermans-MacBook-Pro.local>
2019-12-25 14:48:25 +0700

    add project demo

---
+++ project_demo/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'PROJECT DEMO',
+    'summary': '',
+    'version': '11.0.1.0.0',
+    'category': 'Tools',
+    'author':
+        'Huy Ly',
+    'website': 'https://2wa.vn',
+    'license': 'AGPL-3',
+    'depends': [
+        'base_demo',
+        'project',
+    ],
+    'data': [
+        'data/project_demo.xml',
+    ],
+    'application': True,
+}
