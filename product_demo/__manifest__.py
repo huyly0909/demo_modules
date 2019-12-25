72167e35d60c35de2c248378c9d0425fb185a770
Hitman <hitman@Supermans-MacBook-Pro.local>
2019-12-25 22:01:13 +0700

    product demo (not done)

---
+++ product_demo/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'PRODUCT DEMO',
+    'summary': '',
+    'version': '13.0',
+    'category': 'Tools',
+    'author':
+        'Huy Ly',
+    'website': 'https://2wa.vn',
+    'license': 'AGPL-3',
+    'depends': [
+        'base_demo',
+        'product',
+    ],
+    'data': [
+        'data/product_demo.xml',
+    ],
+    'application': True,
+}
